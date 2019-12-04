02f180bad9c6fd6f237ac33009e08e34fa3680fb
Jon Patterson <JonPatt92@gmail.com>
2019-12-04 15:12:10 -0700

    Add validations for video model
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,10 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  validates :tutorial, :presence => true
+
+  validates_presence_of :title,
+                  :description,
+                    :thumbnail
 end
