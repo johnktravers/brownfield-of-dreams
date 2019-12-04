93ae406947f5c232bd24c5e1393c3d374f3f34a7
Jon Patterson <JonPatt92@gmail.com>
2019-12-04 15:20:41 -0700

    Reorganize routes
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -6,37 +6,37 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'welcome#index'
-  get  'tags/:tag',    to: 'welcome#index', as: :tag
-  get  '/register',    to: 'users#new'
-
   namespace :admin do
-    get '/dashboard',     to: 'dashboard#show'
-    resources :tutorials, only: [:create, :edit, :update, :destroy, :new] do
-      resources :videos,  only: [:create]
-    end
-    resources :videos, only: [:edit, :update, :destroy]
-
     namespace :api do
       namespace :v1 do
         put 'tutorial_sequencer/:tutorial_id', to: 'tutorial_sequencer#update'
       end
     end
+
+    get '/dashboard',                          to: 'dashboard#show'
+
+    resources :videos,    only: [:edit, :update, :destroy]
+    resources :tutorials, only: [:create, :edit, :update, :destroy, :new] do
+      resources :videos,  only: [:create]
+    end
   end
 
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  root 'welcome#index'
+  get  'tags/:tag',                            to: 'welcome#index', as: :tag
+  get  '/register',                            to: 'users#new'
+  get    '/login',                             to: 'sessions#new'
+  post   '/login',                             to: 'sessions#create'
+  delete '/logout',                            to: 'sessions#destroy'
 
-  get '/dashboard',   to: 'users#show'
-  get '/about',       to: 'about#show'
-  get '/get_started', to: 'get_started#show'
+  get '/dashboard',                            to: 'users#show'
+  get '/about',                                to: 'about#show'
+  get '/get_started',                          to: 'get_started#show'
 
   # Is this being used?
   # get '/video', to: 'video#show'
 
-  resources :tutorials, only: [:show, :index] do
-    resources :videos,  only: [:show, :index]
+  resources :tutorials,   only: [:show, :index] do
+    resources :videos,    only: [:show, :index]
   end
 
   resources :users,       only: [:new, :create, :update, :edit]
