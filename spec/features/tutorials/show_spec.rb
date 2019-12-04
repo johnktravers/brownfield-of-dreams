b8d33ac99cea91415f5acc79bef6f34f2590da51
Jon Patterson <JonPatt92@gmail.com>
2019-12-04 16:23:58 -0700

    Build test for tutorial show page
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

---
+++ spec/features/tutorials/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "When I visit a Tutorials show page", type: :feature do
+  it "I see the tutorials videos if it has any" do
+    tutorial = create(:tutorial)
+    create_list(:video, 2, tutorial: tutorial)
+    video_1 = create(:video, tutorial: tutorial)
+    video_2 = create(:video, tutorial: tutorial)
+
+    visit tutorial_path(tutorial)
+
+    expect(page).to have_content(tutorial.title)
+    expect(page).to have_link(video_1.title)
+    expect(page).to have_link(video_2.title)
+  end
+
+  it "Tells me when there are no videos" do
+    tutorial = create(:tutorial)
+
+    visit tutorial_path(tutorial)
+
+    expect(page).to have_content(tutorial.title)
+    expect(page).to have_content("This tutorial has no videos")
+  end
+end
