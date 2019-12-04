ca2401661809325f1ebd9cdf2a4e9813402d8dd5
Jon Patterson <JonPatt92@gmail.com>
2019-12-04 15:08:31 -0700

    Add test for video model
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

---
+++ spec/models/video_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Video, type: :model do
+  describe 'Relationships' do
+    it {should belong_to :tutorial}
+    it {should have_many :user_videos}
+    it {should have_many(:users).through(:user_videos)}
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :thumbnail}
+  end
+end
