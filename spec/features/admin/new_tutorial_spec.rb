cd2afdaf5a1a202c46ed68feeae75147c483c49b
Jon Patterson <JonPatt92@gmail.com>
2019-12-04 16:24:11 -0700

    Add test for admin new_tutorial sad path
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- spec/features/admin/new_tutorial_spec.rb
+++ spec/features/admin/new_tutorial_spec.rb
@@ -19,18 +19,30 @@ describe 'As an Admin.' do
     fill_in 'tutorial[title]', with: 'Tying Shoes'
     fill_in 'tutorial[description]', with: 'How to tie your shoes.'
     fill_in 'tutorial[thumbnail]', with: 'An untied shoe'
-    fill_in 'video[title]', with: 'How to tie your shoes.'
-    fill_in 'video[description]', with: 'Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through.'
-    fill_in 'video[video_id]', with: 'J7ikFUlkP_k'
 
     click_button 'Save'
 
     tutorial = Tutorial.last
-    video = Video.last
 
     expect(current_path).to eq(tutorial_path(tutorial.id))
     expect(page).to have_content('Tutorial was successfully created')
     expect(page).to have_content(tutorial.title)
-    expect(page).to have_link(video.title)
+  end
+
+  it "Doesnt let me leave fields blank" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit new_admin_tutorial_path
+
+    fill_in 'tutorial[title]', with: ''
+    fill_in 'tutorial[description]', with: 'How to tie your shoes.'
+    fill_in 'tutorial[thumbnail]', with: 'An untied shoe'
+
+    click_button 'Save'
+
+    expect(current_path).to eq(admin_tutorials_path)
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content('How to tie your shoes.')
+    expect(page).to have_selector("input[value='An untied shoe']")
   end
 end
