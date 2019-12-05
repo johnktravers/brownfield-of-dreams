8ae853f384ce15c382db993f3bc73de2e60a4a4f
John Travers <johnk.travers@gmail.com>
2019-12-05 11:50:01 -0700

    Remove unnecessary comments and rename test
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- spec/features/admin/new_tutorial_spec.rb
+++ spec/features/admin/new_tutorial_spec.rb
@@ -1,17 +1,9 @@
 require 'rails_helper'
 
-# When I visit '/admin/tutorials/new'
-# And I fill in 'title' with a meaningful name
-# And I fill in 'description' with a some content
-# And I fill in 'thumbnail' with a valid YouTube thumbnail
-# And I click on 'Save'
-# Then I should be on '/tutorials/{NEW_TUTORIAL_ID}'
-# And I should see a flash message that says "Successfully created tutorial."
-
 describe 'As an Admin.' do
   let(:admin)    { create(:admin) }
 
-  it 'Can create a new tutorial with a video' do
+  it 'Can create a new tutorial without a video' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit new_admin_tutorial_path
