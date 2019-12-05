5f6daa112df2a6b63a882ef0446d24b85db2c931
John Travers <johnk.travers@gmail.com>
2019-12-05 16:55:45 -0700

    Initialize github service with github token, validate repo owner using current user
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -1,18 +1,25 @@
 class GithubFacade
+  def initialize(user)
+    @user = user
+  end
 
   def repos
-    service = GithubService.new
+    service = GithubService.new(user.github_token)
     raw_repos_data = service.get_user_repos
 
     repos = []
 
     raw_repos_data.each do |repo_data|
-      repos << Repo.new(repo_data) if repo_data[:owner][:id] == 46035439 # Change to specific username
+      repos << Repo.new(repo_data) if repo_data[:owner][:id] == user.github_id.to_i
       break if repos.length == 5
     end
 
     repos
   end
 
+
+  private
+
+  attr_reader :user
 
 end
