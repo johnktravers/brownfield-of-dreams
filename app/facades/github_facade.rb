69d3930f1b52427cffdbd8542695cee575cca440
John Travers <johnk.travers@gmail.com>
2019-12-06 15:16:03 -0700

    Add memoization, add followers method to create follower objects
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -4,22 +4,31 @@ class GithubFacade
   end
 
   def repos
-    service = GithubService.new(user.github_token)
-    raw_repos_data = service.get_user_repos
+    @raw_repos_data ||= service.get_user_repos
 
     repos = []
-
-    raw_repos_data.each do |repo_data|
+    @raw_repos_data.each do |repo_data|
       repos << Repo.new(repo_data) if repo_data[:owner][:id] == user.github_id.to_i
       break if repos.length == 5
     end
-
     repos
+  end
+
+  def followers
+    @raw_followers_data ||= service.get_user_followers
+
+    @raw_followers_data.map do |follower_data|
+      Follower.new(follower_data)
+    end
   end
 
 
   private
 
   attr_reader :user
+
+  def service
+    GithubService.new(user.github_token)
+  end
 
 end
