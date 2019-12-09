a962cb216d4335233840a6427c66c5ffc11360f9
John Travers <johnk.travers@gmail.com>
2019-12-09 13:16:40 -0700

    Add initial bookmarks method to user model - wip
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
-  has_many :tutorials, through: :videos
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :first_name, :last_name
@@ -14,5 +13,26 @@ class User < ApplicationRecord
       github_id: user_info['uid'],
       github_token: user_info['credentials']['token']
     )
+  end
+
+  def bookmarks
+    results = ActiveRecord::Base.connection.execute(
+      "SELECT tutorials.title as tutorial_title, tutorials.id as tutorial_id, videos.title as video_title, videos.position as video_position
+        FROM videos
+        INNER JOIN tutorials ON videos.tutorial_id = tutorials.id
+        INNER JOIN user_videos ON user_videos.video_id = videos.id
+        WHERE user_videos.user_id = 2
+        ORDER BY tutorial_id, videos.id"
+    )
+
+    bookmarks = []
+
+    tutorials = results.map do |result|
+      [result['tutorial_id'], result['']]
+    end
+
+    tutorials.each do |result|
+      Bookmark.new(result)
+    end
   end
 end
