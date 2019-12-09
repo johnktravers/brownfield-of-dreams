61b44d94c11d89950d5e49fd2f06ebc43667552f
Jon Patterson <JonPatt92@gmail.com>
2019-12-09 16:07:09 -0700

    Build bookmark poro, nested with video poros created in the add_videos method
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- app/models/poros/bookmark.rb
+++ app/models/poros/bookmark.rb
@@ -1,12 +1,13 @@
 class Bookmark
-  attr_reader :tutorial_title, :videos
+  attr_reader :tutorial_title, :tutorial_id, :videos
 
   def initialize(attrs)
-    @tutorial_title = attrs['tutorial_title']
+    @tutorial_id = attrs[0]
+    @tutorial_title = attrs[1]
     @videos = []
   end
 
-  def add_video(video_title, video_position)
-    videos << [video_title, video_position]
+  def add_video(results)
+    @videos << BookmarkVideo.new(results)
   end
 end
