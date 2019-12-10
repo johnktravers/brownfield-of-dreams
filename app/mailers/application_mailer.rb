2bd842c214d5c27661f76c7d0b4c997d91eebf22
John Travers <johnk.travers@gmail.com>
2019-12-10 14:11:35 -0700

    Change the default from email address to the Heroku app domain
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@brownfield-of-dreams.com'
+  default from: 'no-reply@brownfield-of-dreams-1908.herokuapp.com'
   layout 'mailer'
 end
