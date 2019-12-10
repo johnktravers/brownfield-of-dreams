cf81b91f842c6fd0bfe497795b347f7c0d7e6d5d
John Travers <johnk.travers@gmail.com>
2019-12-10 16:42:26 -0700

    Add test to verify user mailer activation mail delivery
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -1,5 +1,30 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#activation' do
+    let(:user) { create(:user) }
+    let(:mail) { described_class.activation(user).deliver_now }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Activate Your Account')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['no-reply@brownfield-of-dreams-1908.herokuapp.com'])
+    end
+
+    it 'assigns @user.first_name' do
+      expect(mail.body.encoded).to include(user.first_name)
+    end
+
+    it 'assigns @user.activation_token' do
+      expect(mail.body.encoded)
+        .to include("https://brownfield-of-dreams-1908.herokuapp.com/activate?activation_token=#{user.activation_token}")
+    end
+  end
 end
