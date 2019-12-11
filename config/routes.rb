a34ea5fd49030a5eb0df4fcf485f2ef1698d3487
Jon Patterson <JonPatt92@gmail.com>
2019-12-11 12:47:22 -0700

    Delete unused routes
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,38 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :tutorials, only: [:show, :index]
-      resources :videos,    only: [:show]
+      resources :tutorials,     only: [:show, :index]
+      resources :videos,        only: [:show]
     end
   end
 
   namespace :admin do
-    namespace :api do
-      namespace :v1 do
-        put 'tutorial_sequencer/:tutorial_id', to: 'tutorial_sequencer#update'
-      end
-    end
+    get '/dashboard',           to: 'dashboard#show'
 
-    get '/dashboard',                          to: 'dashboard#show'
-
-    resources :videos,    only: [:edit, :update, :destroy]
-    resources :tutorials, except: [:index, :show] do
-      resources :videos,  only: [:create]
+    resources :videos,          only: [:edit, :update, :destroy]
+    
+    resources :tutorials,       except: [:index, :show] do
+      resources :videos,        only: [:create]
     end
   end
+
+  resources :tutorials,         only: [:show, :index]
+  resources :users,             only: [:new, :create, :update, :edit]
+  resources :user_videos,       only: [:create, :destroy]
 
   root 'welcome#index'
-  get  'tags/:tag',                            to: 'welcome#index', as: :tag
-  get  '/register',                            to: 'users#new'
-  get    '/login',                             to: 'sessions#new'
-  post   '/login',                             to: 'sessions#create'
-  delete '/logout',                            to: 'sessions#destroy'
+  get  'tags/:tag',             to: 'welcome#index', as: :tag
+  get  '/register',             to: 'users#new'
+  get    '/login',              to: 'sessions#new'
+  post   '/login',              to: 'sessions#create'
+  delete '/logout',             to: 'sessions#destroy'
 
-  get '/dashboard',                            to: 'users#show'
-  get '/activate',                             to: 'users#update'
-  get '/about',                                to: 'about#show'
-  get '/get_started',                          to: 'get_started#show'
-  get '/invite',                               to: 'invite#new'
-  post '/invite',                         to: 'invite#create'
-
-  # Is this being used?
-  # get '/video', to: 'video#show'
-
-  resources :tutorials,   only: [:show, :index] do
-    resources :videos,    only: [:show, :index]
-  end
-
-  resources :users,       only: [:new, :create, :update, :edit]
-  resources :user_videos, only: [:create, :destroy]
-
-  get '/auth/github',          as: 'connect_github'
-  get '/auth/github/callback', to: 'github_connections#create'
+  get '/dashboard',             to: 'users#show'
+  get '/activate',              to: 'users#update'
+  get '/about',                 to: 'about#show'
+  get '/get_started',           to: 'get_started#show'
+  get '/invite',                to: 'invite#new'
+  post '/invite',               to: 'invite#create'
+  get '/auth/github',           as: 'connect_github'
+  get '/auth/github/callback',  to: 'github_connections#create'
 end
