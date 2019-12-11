46722e4d0e63212ed43f37ee88fd7ee979b26c6f
Jon Patterson <JonPatt92@gmail.com>
2019-12-11 16:05:43 -0700

    Build tests for adding friends from your followers and folowing
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- spec/features/user/friendships_spec.rb
+++ spec/features/user/friendships_spec.rb
@@ -23,24 +23,70 @@ RSpec.describe 'User Friendships' do
       within('#following-id-51488670') do
         expect(page).to_not have_button('Add as Friend')
       end
+
+      # Sad path for FriendshipController line #9
     end
   end
 
   it 'can add a github follower as a friend if they have an account' do
-    VCR.use_cassette('github_user_data') do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@john)
+    VCR.use_cassette('twofold_github_user_data') do
+      visit login_path
 
-      visit dashboard_path
+      fill_in 'session[email]', with: @john.email
+      fill_in 'session[password]', with: @john.password
+
+      click_button 'Log In'
+
+      expect(current_path).to eq(dashboard_path)
 
       within('#follower-id-47950816') do
         click_button('Add as Friend')
       end
 
       expect(current_path).to eq(dashboard_path)
+
+      @john.reload
+
       within('.friends') do
         expect(page).to have_content('Jonpatt92')
+      end
+
+      expect(page).to have_content("Yay, you added #{@jonathan.first_name} #{@jonathan.last_name} as a friend!")
+
+      within('#follower-id-47950816') do
+        expect(page).to_not have_button('Add as Friend')
       end
     end
   end
 
+  it 'can add a github following as a friend if they have an account' do
+    VCR.use_cassette('twofold_github_user_data') do
+      visit login_path
+
+      fill_in 'session[email]', with: @john.email
+      fill_in 'session[password]', with: @john.password
+
+      click_button 'Log In'
+
+      expect(current_path).to eq(dashboard_path)
+
+      within('#following-id-47950816') do
+        click_button('Add as Friend')
+      end
+
+      expect(current_path).to eq(dashboard_path)
+
+      @john.reload
+
+      within('.friends') do
+        expect(page).to have_content('Jonpatt92')
+      end
+
+      expect(page).to have_content("Yay, you added #{@jonathan.first_name} #{@jonathan.last_name} as a friend!")
+
+      within('#following-id-47950816') do
+        expect(page).to_not have_button('Add as Friend')
+      end
+    end
+  end
 end
