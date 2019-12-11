de103fc352ab2a606152f82e32540da43cd84538
John Travers <johnk.travers@gmail.com>
2019-12-11 16:47:36 -0700

    Render 404 page if user with given friend id is not found
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,13 +2,14 @@ class FriendshipsController < ApplicationController
 
   def create
     friendship = current_user.friendships.new(friend_id: params[:friend_id])
-    new_friend = User.find(friendship.friend_id)
+    new_friend = User.find_by(id: friendship.friend_id)
+
     if new_friend && friendship.save
       flash[:success] = "Yay, you added #{new_friend.first_name} #{new_friend.last_name} as a friend!"
+      redirect_to dashboard_path
     else
-      flash[:notice] = "That user doesn't exist"
+      render file: 'public/404', status: :not_found
     end
-    redirect_to dashboard_path
   end
 
 end
